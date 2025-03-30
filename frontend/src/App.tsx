56c74a30cb869449c66a54c638443f2b48757410
JWalczak123 <145325286+JWalczak123@users.noreply.github.com>
2025-03-31 00:47:44 +0200

    Modify some lines in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './index.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         Strona główna
@@ -13,5 +7,4 @@ function App() {
     </>
   )
 }
-
 export default App
