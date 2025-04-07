7a7b36c17a5e49ccd97cf7e019afdf9b7f890f81
JWalczak123 <jakub.walczak@uczen.zsk.poznan.pl>
2025-04-07 13:02:45 +0200

    Change in structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './routes/App.tsx'
+import AppRouter from './routes/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <AppRouter />
