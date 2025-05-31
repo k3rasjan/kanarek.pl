a7d8f7be9dce8f0f468f1f631767e4c50cd67587
k3rasjan <mateusz.najsarek@gmail.com>
2025-05-31 22:30:34 +0200

    simba commit

--- backend/src/socket/index.ts
+++ backend/src/socket/index.ts
@@ -1,31 +1,28 @@
-import { Server } from 'socket.io';
-import { getVehiclesWithInspectorInfo } from '@helpers/vehiclePositions';
-import { vehicleStore } from '@helpers/stores';
+import { Server } from "socket.io";
+import { getVehiclesWithInspectorInfo } from "@helpers/vehiclePositions";
+import { vehicleStore } from "@helpers/stores";
 
 export function initializeSocket(io: Server) {
-  // Set the socket.io instance in the store
   vehicleStore.setSocketIO(io);
 
-  // Emit vehicle positions every 10 seconds
   setInterval(async () => {
     try {
       const vehicles = await getVehiclesWithInspectorInfo();
-      io.emit('vehiclePositions', vehicles);
+      io.emit("vehiclePositions", vehicles);
     } catch (error) {
-      console.error('Error emitting vehicle positions:', error);
+      console.error("Error emitting vehicle positions:", error);
     }
   }, 10000);
 
-  // Handle socket connections
-  io.on('connection', (socket) => {
-    console.log('Client connected');
+  io.on("connection", (socket) => {
+    console.log("Client connected");
 
-    // Send initial vehicle data to the new client
     const vehicles = vehicleStore.getVehicles();
-    socket.emit('vehiclePositions', "SAdasd");
+    socket.emit("vehiclePositions", "SAdasd");
 
-    socket.on('disconnect', () => {
-      console.log('Client disconnected');
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
     });
   });
-} 
+}
+
