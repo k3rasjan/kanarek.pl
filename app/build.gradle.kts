48ab06b3a1b3090187ff139b9199d4ebde38a114
JWalczak123 <jakub.walczak@zsk.poznan.pl>
2025-06-03 23:55:48 +0200

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.android.gms:play-services-location:21.0.1")
-    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("com.google.android.material:material:1.12.0")
     implementation ("androidx.core:core:1.12.0")
 
 }
